d68ffeda15293a68391a9eda011a3f9a55d896ab
lihiaviram <lihiviram@gmail.com>
2025-12-01 22:44:03 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -2,7 +2,7 @@ def create_codon_dict(file_path):
     split_acids={}
      with open(file_path) as codons_file:
            for row in codons_file.readlines():
-               row.strip().split('\t')
+               row=row.strip().split('\t')
                split_acids={row[0]:row[2]}
     return split_acids
         
